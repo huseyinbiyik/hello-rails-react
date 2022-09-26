9477e9977e87f79f689578369def737d2e5d6849
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-26 18:29:20 +0300

    Add root model and app component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
